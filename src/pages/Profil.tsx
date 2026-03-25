d1bd102604f284ebe9bb805d72a46192907ed42d
beaubeau406 <beauedikou@gmail.com>
2026-03-25 18:26:49 +0100

    Update Profil.tsx

--- src/pages/Profil.tsx
+++ src/pages/Profil.tsx
@@ -34,7 +34,7 @@ const Profil = () => {
           <div className="w-10 h-10 rounded-xl grad-yellow flex items-center justify-center"><Target className="w-5 h-5 text-white" /></div>
           <div>
             <p className="text-yellow-700 text-xs font-medium">Objectif principal</p>
-            <p className="text-yellow-900 font-bold text-lg">{eleve.Objectif || 'Non défini'}</p>
+            <p className="text-yellow-900 font-bold text-lg">{eleve.Objectifs || 'Non défini'}</p>
           </div>
         </div>
         <div className="space-y-2">
